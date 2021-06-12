892fd75bc59c2d2933563d23d036e33909876f0a
AlexKoposov <cmango67@gmail.com>
2021-06-12 21:32:16 +0500

    Изменена плавность появления текста

--- HybridHarvestProj/Assets/Scripts/AnimateText.cs
+++ HybridHarvestProj/Assets/Scripts/AnimateText.cs
@@ -8,22 +8,20 @@ public class AnimateText : MonoBehaviour
     [SerializeField] Text input;
     string currentText;
     int frame, frequency;
-    string[] splited;
 
     // Start is called before the first frame update
     void Start()
     {
         currentText = input.text;
-        splited = currentText.Split();
         input.text = "";
-        frequency = 20;
+        frequency = 15;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (frame++ % frequency == 0 && frame / frequency < splited.Length)
-            input.text += $"{splited[frame / frequency]} ";
+        if (frame++ % frequency == 0 && frame / frequency < currentText.Length)
+            input.text += $"{currentText[frame / frequency]}";
     }
 
     void OnDisable()
