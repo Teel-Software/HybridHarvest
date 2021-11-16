0ecc7f4f2b4955e48ca5e3df9f6be7cbce61caa0
Alexey <cmango67@gmail.com>
2021-11-16 14:19:57 +0500

    Добавил инструмент для создания диалогов
    Пока без вариантов ответа

---
+++ HybridHarvest/Assets/Scripts/DialogPanelLogic.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Определяет говорящего в текущий момент персонажа
+/// </summary>
+public enum NowTalking
+{
+    Narrator,
+    First,
+    Second
+}
+
+/// <summary>
+/// Содержит персонажа и его речь
+/// </summary>
+internal class Speech
+{
+    /// <summary>
+    /// Содержит персонажа и его речь
+    /// </summary>
+    public Speech(NowTalking character, string phrase)
+    {
+        Character = character;
+        Phrase = phrase;
+    }
+
+    public NowTalking Character { get; private set; }
+    public string Phrase { get; private set; }
+}
+
+public class DialogPanelLogic : MonoBehaviour
+{
+    [SerializeField] Image CharacterSpritePlace;
+
+    private Sprite FirstCharacterSprite;
+    private Sprite SecondCharacterSprite;
+    private Sprite NarratorSprite;
+    private List<Speech> scenario;
+    private Dictionary<string, Speech> answers;
+    private int speechIndex;
+
+    /// <summary>
+    /// Готовит панель к показу диалога, следует вызывать этот метод первым
+    /// </summary>
+    public void CreateDialogPanel(Sprite firstCharacterSprite, Sprite secondCharacterSprite, Sprite narratorSprite = null)
+    {
+        scenario = new List<Speech>();
+        answers = new Dictionary<string, Speech>();
+        FirstCharacterSprite = firstCharacterSprite;
+        SecondCharacterSprite = secondCharacterSprite;
+        NarratorSprite = narratorSprite;
+    }
+
+    /// <summary>
+    /// Добавляет фразу в конец диалога
+    /// </summary>
+    public void AddPhrase(NowTalking character, string phrase, string answerOn = "")
+    {
+        var speech = new Speech(character, phrase);
+
+        if (answerOn == "")
+            scenario.Add(speech);
+        else answers[answerOn] = speech;
+    }
+
+    /// <summary>
+    /// Запускает диалог, следует вызывать этот метод последним
+    /// </summary>
+    public void StartDialog()
+    {
+        speechIndex = -1;
+        LoadNewPhrase();
+        transform.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// Выводит следующую (если присутствует) фразу на диалоговою панель, если фразы нет - деактивирует панель
+    /// </summary>
+    public void LoadNewPhrase()
+    {
+        if (++speechIndex >= scenario.Count)
+        {
+            transform.gameObject.SetActive(false);
+            return;
+        };
+
+        var currentSpeech = scenario[speechIndex];
+        CharacterSpritePlace.sprite =
+            currentSpeech.Character == NowTalking.First
+            ? FirstCharacterSprite
+            : currentSpeech.Character == NowTalking.Second
+                ? SecondCharacterSprite
+                : NarratorSprite;
+        transform.gameObject.GetComponentInChildren<Text>().text = currentSpeech.Phrase;
+    }
+}
---
+++ HybridHarvest/Assets/Scripts/Scenario.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Scenario : MonoBehaviour
+{
+    [SerializeField] DialogPanelLogic DialogPanel;
+    [SerializeField] Sprite FirstCharacterSprite;
+    [SerializeField] Sprite SecondCharacterSprite;
+    [SerializeField] Sprite NarratorSprite;
+
+    /// <summary>
+    /// В подобных методах нужно создавать диалоги
+    /// </summary>
+    public void MakeTestDialog()
+    {
+        DialogPanel.CreateDialogPanel(FirstCharacterSprite, SecondCharacterSprite, NarratorSprite);
+
+        DialogPanel.AddPhrase(NowTalking.Second, "Приветствую путник, вижу ты первый раз здесь.");
+        DialogPanel.AddPhrase(NowTalking.First, "Эээээ, ну здрасьте!");
+        DialogPanel.AddPhrase(NowTalking.Second, "Я - Великий Стонкс и у меня есть всевозможные товары, начиная с огурца и заканчивая морковью.");
+        DialogPanel.AddPhrase(NowTalking.First, "Хочется взять что-то необычное... Я думаю, огурец выглядит неплохо, поэтому возьму Иерусалим.");
+        DialogPanel.AddPhrase(NowTalking.Second, "???????????????\n[Великий Стонкс помер от такого]");
+        DialogPanel.AddPhrase(NowTalking.Narrator, "Со смертью этого персонажа нить повествования обрывается.");
+
+        DialogPanel.StartDialog();
+    }
+}
