5c534b31b133c02b7770328f893983cf16e8b857
Teelirium <74907594+Teelirium@users.noreply.github.com>
2021-12-05 15:22:39 +0500

    Улучшено представление биржи

--- HybridHarvest/Assets/Scripts/UI/MarketMenu.cs
+++ HybridHarvest/Assets/Scripts/UI/MarketMenu.cs
@@ -8,10 +8,6 @@ public class MarketMenu : MonoBehaviour
 { 
     [SerializeField] public GridLayoutGroup ScrollList;
     [SerializeField] public GameObject Listing;
-    public void Awake()
-    {
-        
-    }
 
     public void OnEnable()
     {
@@ -20,8 +16,18 @@ public class MarketMenu : MonoBehaviour
             var objName = $"{seedName}Multiplier";
             var listing = Instantiate(Listing, ScrollList.transform);
             listing.name = objName;
-            listing.GetComponentInChildren<Image>().sprite = Resources.Load<Sprite>($"SeedsIcons\\{seedName}");
-            listing.GetComponentInChildren<Text>().text = $"x {Market.PriceMultipliers[seedName]}";
+            
+            listing.GetComponentsInChildren<Image>()[1].sprite = Resources.Load<Sprite>($"SeedsIcons\\{seedName}");
+            
+            var txtComponent = listing.GetComponentInChildren<Text>();
+            var multiplier = Market.PriceMultipliers[seedName];
+            txtComponent.text = $"x {multiplier}";
+            txtComponent.color = 
+                multiplier == 1.0f ? 
+                    new Color(0,0,0) : 
+                multiplier > 1.0f ? 
+                    new Color(0f, 0.76f, 0.02f) : 
+                new Color(0.75f, 0.16f, 0.13f);
         }
     }
 
