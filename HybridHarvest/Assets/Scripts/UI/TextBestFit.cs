ef40860b0594920e6c2ce42627e7652666b663ec
Alexey <cmango67@gmail.com>
2021-12-12 16:38:00 +0500

    Добавил скрипт для автоматического подбора размера текста
    - Изменил названия триггеров в аниматоре слайдов на более понятные
    - Поменял LineSpacing у шрифта MINECRAFT (если сломается отображение некоторых текстов - в самом компоненте Text надо поставить LineSpacing = 1)
    - Обновил спрайт картошки

---
+++ HybridHarvest/Assets/Scripts/UI/TextBestFit.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBestFit : MonoBehaviour
+{
+    [SerializeField] int minSize;
+    [SerializeField] int maxSize;
+
+    Text currTextComp;
+    RectTransform currRT;
+
+    /// <summary>
+    /// Находит текущий компонент "Text"
+    /// </summary>
+    void Start()
+    {
+        currTextComp = gameObject.GetComponent<Text>();
+        currRT = gameObject.GetComponent<RectTransform>();
+    }
+
+    /// <summary>
+    /// Изменяет размер шрифта согласно заданным параметрам
+    /// </summary>
+    void Update()
+    {
+        var tHeight = currTextComp.preferredHeight;
+        var rtHeight = currRT.rect.height;
+        var fSize = currTextComp.fontSize;
+
+        // уменьшает шрифт
+        if (tHeight > rtHeight || fSize > maxSize)
+        {
+            var newFontSize = fSize - 1;
+            while (tHeight > rtHeight || newFontSize >= maxSize)
+            {
+                if (newFontSize >= minSize)
+                    currTextComp.fontSize = newFontSize--;
+                else break;
+                tHeight = currTextComp.preferredHeight;
+            }
+        }
+        // увеличивает шрифт
+        else if (tHeight < rtHeight || fSize < minSize)
+        {
+            var newFontSize = fSize + 1;
+            while (tHeight <= rtHeight)
+            {
+                if (newFontSize <= maxSize)
+                    currTextComp.fontSize = newFontSize++;
+                else break;
+                tHeight = currTextComp.preferredHeight;
+            }
+
+            if (tHeight > rtHeight)
+                currTextComp.fontSize--;
+        }
+    }
+}
