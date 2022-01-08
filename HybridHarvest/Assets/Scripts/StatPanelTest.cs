15c3fff5ce97b998cb0d25ac4ab9336150de6c86
Teelirium <angryzombi01@gmail.com>
2022-01-08 16:36:07 +0500

    Основные статы растения на панельке

--- HybridHarvest/Assets/Scripts/StatPanelTest.cs
+++ HybridHarvest/Assets/Scripts/StatPanelTest.cs
@@ -13,9 +13,10 @@ public class StatPanelTest : MonoBehaviour
         var rand = new Random();
         var num = rand.Next(0, inv.Elements.Count);
         
-        var seedItem = inv.Elements[num];
         var stat = Instantiate(StatPanel, GameObject.Find("Canvas").transform);
         var statDrawer = stat.GetComponentInChildren<StatPanelDrawer>();
-        statDrawer.PlantImage.sprite = seedItem.PlantSprite;
+        
+        var seedItem = inv.Elements[num];
+        statDrawer.DisplayStats(seedItem);
     }
 }
--- HybridHarvest/Assets/Scripts/UI/StatPanelDrawer.cs
+++ HybridHarvest/Assets/Scripts/UI/StatPanelDrawer.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 
 public class StatPanelDrawer : MonoBehaviour
 {
-    [SerializeField] public Image PlantImage;
-    [SerializeField] public TextMeshProUGUI PlantDesc;
+    [SerializeField] private Image PlantImage;
+    [SerializeField] private TextMeshProUGUI PlantDesc;
+    [SerializeField] private Text PlantName;
+    [SerializeField] private TextMeshProUGUI PlantNameLatin;
+
     void Start()
     {
         
@@ -16,5 +19,18 @@ public class StatPanelDrawer : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void DisplayStats(Seed seed)
+    {
+        PlantImage.sprite = seed.PlantSprite;
+        
+        PlantName.text = seed.NameInRussian;
+        PlantNameLatin.text = $"(лат. {seed.NameInLatin})";
+        
+        PlantDesc.text = $"Вкус - {seed.Taste}\n\n" +
+                         $"Габитус - {seed.Gabitus}\n\n" +
+                         $"Время роста - {seed.GrowTime} секунд\n\n" +
+                         $"Цена - {seed.Price}<sprite name=\"Money\">\n";
     }
 }
