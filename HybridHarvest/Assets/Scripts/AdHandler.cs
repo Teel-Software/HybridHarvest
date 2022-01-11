a0051cae1ba6c9155f343bd837d20f77e7cfc737
Teelirium <angryzombi01@gmail.com>
2022-01-11 17:16:06 +0500

    Рекламка

--- HybridHarvest/Assets/Scripts/AdHandler.cs
+++ HybridHarvest/Assets/Scripts/AdHandler.cs
@@ -1,14 +1,11 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
 
-public class AdHandler : MonoBehaviour
+public class AdHandler : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
 {
-    private Inventory _inventory;
-
-    private void Start()
-    {
-        _inventory = GameObject.Find("DataKeeper").GetComponent<Inventory>();
-    }
 
     public void UpdateEnergy()
     {
@@ -18,5 +15,100 @@ public class AdHandler : MonoBehaviour
     public void DebugResetEnergy()
     {
         _inventory.ConsumeEnergy(_inventory.Energy);
+    }
+    
+    [SerializeField] Button _showAdButton;
+    [SerializeField] string _androidAdUnitId = "Rewarded_Android";
+    [SerializeField] string _iOSAdUnitId = "Rewarded_iOS";
+    private string _adUnitId;
+    
+    private Inventory _inventory;
+    void Awake()
+    {
+        _inventory ??= GameObject.Find("DataKeeper").GetComponent<Inventory>();
+        // Get the Ad Unit ID for the current platform
+        _adUnitId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOSAdUnitId
+            : _androidAdUnitId;
+        //Disable button until ad is ready to show
+        _showAdButton.interactable = false;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(WaitForLoad());
+    }
+    
+    // Crutch to guarantee sdk initialization
+    private IEnumerator WaitForLoad()
+    {
+        yield return new WaitForSeconds(1f);
+        LoadAd();
+    }
+    
+    // Load content to the Ad Unit:
+    public void LoadAd()
+    {
+        // IMPORTANT! Only load content AFTER initialization (in this example, initialization is handled in a different script).
+        Debug.Log("Loading Ad: " + _adUnitId);
+        Advertisement.Load(_adUnitId, this);
+    }
+ 
+    // If the ad successfully loads, add a listener to the button and enable it:
+    public void OnUnityAdsAdLoaded(string adUnitId)
+    {
+        Debug.Log("Ad Loaded: " + adUnitId);
+ 
+        if (adUnitId.Equals(_adUnitId))
+        {
+            // Configure the button to call the ShowAd() method when clicked:
+            _showAdButton.onClick.AddListener(ShowAd);
+            // Enable the button for users to click:
+            _showAdButton.interactable = true;
+        }
+    }
+ 
+    // Implement a method to execute when the user clicks the button.
+    public void ShowAd()
+    {
+        // Disable the button: 
+        _showAdButton.interactable = false;
+        // Then show the ad:
+        Advertisement.Show(_adUnitId, this);
+    }
+ 
+    // Implement the Show Listener's OnUnityAdsShowComplete callback method to determine if the user gets a reward:
+    public void OnUnityAdsShowComplete(string adUnitId, UnityAdsShowCompletionState showCompletionState)
+    {
+        if (adUnitId.Equals(_adUnitId) && showCompletionState.Equals(UnityAdsShowCompletionState.COMPLETED))
+        {
+            Debug.Log("Unity Ads Rewarded: Ad Completed");
+            // Grant a reward.
+            UpdateEnergy();
+            // Load another ad:
+            Advertisement.Load(_adUnitId, this);
+        }
+    }
+ 
+    // Implement Load and Show Listener error callbacks:
+    public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit {adUnitId}: {error.ToString()} - {message}");
+        // Use the error details to determine whether to try to load another ad.
+    }
+ 
+    public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+        // Use the error details to determine whether to try to load another ad.
+    }
+ 
+    public void OnUnityAdsShowStart(string adUnitId) { }
+    public void OnUnityAdsShowClick(string adUnitId) { }
+ 
+    void OnDestroy()
+    {
+        // Clean up the button listeners:
+        _showAdButton.onClick.RemoveAllListeners();
     }
 }
