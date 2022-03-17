9b4a778aa46d99ac8a279abcad08ab15504e0cb4
Teelirium <angryzombi01@gmail.com>
2022-03-17 17:21:17 +0500

    визуально от 1 до 3 кнопок на выставке

--- HybridHarvest/Assets/Scripts/Exhibition/ExhibitionBehaviour.cs
+++ HybridHarvest/Assets/Scripts/Exhibition/ExhibitionBehaviour.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = System.Random;
 
 public class ExhibitionBehaviour : MonoBehaviour
 {
     [SerializeField] public Button[] exhButtons;
-    private DateTime date;
 
     void OnEnable()
     {
-        date = DateTime.Now;
-        foreach (var btn in exhButtons)
+        var epic = false;
+        if (epic)
+        {
+            foreach (var btn in exhButtons)
+                btn.gameObject.SetActive(true);
+            var rand = new Random();
+            var count = rand.Next(0, 3);
+            for (var i = 1; i <= count; i++)
+                exhButtons[exhButtons.Length - i].gameObject.SetActive(false);
+        }
+
+        var date = DateTime.Now;
+        foreach (var btn in exhButtons.Where(btn => btn.IsActive()))
         {
             btn.onClick.RemoveAllListeners();
             switch (date.DayOfWeek)
