d69d7745a6fb0fb04e8d474256e3764c63229a8d
Alexey Koposov <cmango67@gmail.com>
2022-05-25 18:38:05 +0500

    Добавлен спрайт, оповещающий о недостатке уровня

--- HybridHarvest/Assets/Scripts/Tools/OpenOnLevel.cs
+++ HybridHarvest/Assets/Scripts/Tools/OpenOnLevel.cs
@@ -4,9 +4,13 @@ using UnityEngine.UI;
 public class OpenOnLevel : MonoBehaviour
 {
     [SerializeField] private int level;
+    [SerializeField] private Image closedAreaPrefab;
+
     private Inventory inventory;
     private Button btnComp;
     private Button.ButtonClickedEvent originOnClick;
+    private Image placeholder;
+    private bool phIsDestroyed;
     private bool isButton => btnComp != null;
 
     private void Start()
@@ -16,10 +20,13 @@ public class OpenOnLevel : MonoBehaviour
 
         if (!isButton) return;
 
+        if (inventory.Level < level && closedAreaPrefab != null)
+            placeholder = Instantiate(closedAreaPrefab, transform, false);
+
         var notif = GameObject.FindGameObjectWithTag("Inventory").GetComponent<NotificationCenter>();
         originOnClick = btnComp.onClick;
         btnComp.onClick = new Button.ButtonClickedEvent();
-        
+
         btnComp.onClick.AddListener(() =>
         {
             if (inventory.Level >= level)
@@ -29,17 +36,23 @@ public class OpenOnLevel : MonoBehaviour
                 btnComp.onClick.Invoke();
             }
             else
+            {
                 notif.Show($"Откроется на уровне {level}.");
+            }
         });
     }
 
-    // /// <summary>
-    // /// Делает кнопку активной при достижении определённого уровня.
-    // /// </summary>
-    // private void Update()
-    // {
-    //     if (!isButton) return;
-    //     
-    //     btnComp.interactable = inventory.Level >= level;
-    // }
+    /// <summary>
+    /// Делает кнопку активной при достижении определённого уровня.
+    /// </summary>
+    private void Update()
+    {
+        if (!isButton
+            || phIsDestroyed
+            || inventory.Level < level)
+            return;
+
+        Destroy(placeholder);
+        phIsDestroyed = true;
+    }
 }
